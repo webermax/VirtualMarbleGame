d26767dd4b8c49ca87e9b6c98baa704dd77f3be1
Maximilian Weber <post@wbrmx.de>
2012-01-12 20:25:05 +0100

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  VirtualMarbleGame
+//
+//  Created by Maximilian Weber on 12/01/2012.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
